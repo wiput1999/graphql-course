34edc6d6d4a4b8d615697dc6e5af01866b6ceabc
Wiput Pootong <wiput.pootong@gmail.com>
2019-12-18 19:35:18 +0700

    :sparkles: add company root queries Section 04 Lesson 020

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -53,6 +53,13 @@ const RootQuery = new GraphQLObjectType({
       resolve(parentValue, args) {
         return axios.get(`http://localhost:3000/users/${args.id}`).then(response => response.data)
       }
+    },
+    company: {
+      type: CompanyType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/companies/${args.id}`).then(response => response.data)
+      }
     }
   }
 })
